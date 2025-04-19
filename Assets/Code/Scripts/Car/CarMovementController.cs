55ebc6dae7fd7c4d255f8cc9e88159dc91ebae6f
tiago-xavier-braga <braga.tiagoxavier@gmail.com>
2025-04-19 10:20:05 -0300

    feagt-code: Created the code to drifting car

--- Assets/Code/Scripts/Car/CarMovementController.cs
+++ Assets/Code/Scripts/Car/CarMovementController.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 using XaviEssencials.Runtime;
 
@@ -10,13 +12,16 @@ namespace XaviGames.Car
     {
         [Header("Car Properties")]
         [SerializeField]
+        private CarParameter _carParameter;
+        
+        [SerializeField]
         private PlayerInput _playerInput;
 
         [SerializeField]
-        private CarManager _carManager;
+        private Rigidbody _rigidBody;
 
         [SerializeField]
-        private Rigidbody _rigidBody;
+        private List<WheelController> _wheelControllers;
 
         [Header("Info")]
         [SerializeField]
@@ -25,36 +30,40 @@ namespace XaviGames.Car
 
         [SerializeField]
         [ReadOnly]
+        private bool _inputHandbrake;
+
+        [SerializeField]
+        [ReadOnly]
         private float _kmPerHour = 0f;
 
-        private CarParameter _carParameter;
+        private WheelFrictionCurve _defaultSidewaysFriction = new();
+        private WheelFrictionCurve _driftSidewaysFriction = new();
+        
+        private float _defaultAngularDamping;
+
+        [SerializeField]
+        private float _driftAngularDamping;
+
+        [Space]
+        [Header("Debug")]
+        [SerializeField]
+        private UnityEvent OnEnableTrail;
+
+        [SerializeField]
+        private UnityEvent OnDisableTrail;
 
         public override void OnNetworkSpawn()
         {
-            _carParameter = _carManager.CarParameter;
-
-            Vector3 centerOfMass = _rigidBody.centerOfMass;
-            centerOfMass.y += _carParameter.CentreOfGravityOffset;
-            _rigidBody.centerOfMass = centerOfMass;
-
             _playerInput.enabled = IsOwner;
+            ApplyCenterMass();
+            ConfigureWheelSettings();
+
             base.OnNetworkSpawn();
         }
 
         private void FixedUpdate()
         {
-            _kmPerHour = _rigidBody.linearVelocity.magnitude * 3.6f;
-
-            float forwardSpeed = Vector3.Dot(transform.forward, _rigidBody.linearVelocity);
-            float speedFactor = Mathf.InverseLerp(0, _carParameter.TopSpeed, Mathf.Abs(forwardSpeed));
-
-            float currentMotorTorque = Mathf.Lerp(_carParameter.Acceleration, 0, speedFactor);
-            float currentSteerRange = Mathf.Lerp(
-                _carParameter.SteeringRange, _carParameter.SteeringRangeAtMaxSpeed, speedFactor);
-
-            bool isAccelerating = Mathf.Sign(_inputVector.y) == Mathf.Sign(forwardSpeed);
-
-            ApplyWheelForces(currentMotorTorque, currentSteerRange, isAccelerating);
+            UpdateCarPhysics();
         }
 
         public void OnMoveInput(InputAction.CallbackContext context)
@@ -67,9 +76,87 @@ namespace XaviGames.Car
             _inputVector = context.ReadValue<Vector2>();
         }
 
+        public void OnHandbrake(InputAction.CallbackContext context)
+        {
+            if (!IsOwner)
+            {
+                return;
+            }
+
+            switch (context.phase)
+            {
+                case InputActionPhase.Performed:
+                    ApplyDriftWheelSettings();
+                    break;
+
+                case InputActionPhase.Canceled:
+                    ApplyDefaultWheelSettings();
+                    break;
+            }
+        }
+        private void ApplyCenterMass()
+        {
+            Vector3 centerOfMass = _rigidBody.centerOfMass;
+            centerOfMass.y += _carParameter.CentreOfGravityOffset;
+            _rigidBody.centerOfMass = centerOfMass;
+        }
+
+        private void ConfigureWheelSettings()
+        {
+            _defaultSidewaysFriction = _wheelControllers.First().WheelCollider.sidewaysFriction;
+
+            _driftSidewaysFriction = _carParameter.DriftFrictionCurve;
+            _defaultAngularDamping = _rigidBody.angularDamping;
+            _driftAngularDamping = 0.3f;
+        }
+
+
+        private void ApplyDriftWheelSettings()
+        {
+            foreach (var wheel in _wheelControllers)
+            {
+                wheel.WheelCollider.sidewaysFriction = _driftSidewaysFriction;
+            }
+
+            _rigidBody.angularDamping = _driftAngularDamping;
+
+            OnEnableTrail?.Invoke();
+        }
+
+        private void ApplyDefaultWheelSettings()
+        {
+            foreach (var wheel in _wheelControllers)
+            {
+                wheel.WheelCollider.sidewaysFriction = _defaultSidewaysFriction;
+            }
+
+            _rigidBody.angularDamping = _defaultAngularDamping;
+
+            OnDisableTrail?.Invoke();
+        }
+
+        private void UpdateCarPhysics()
+        {
+            _kmPerHour = _rigidBody.linearVelocity.magnitude * 3.6f;
+
+            float forwardSpeed = Vector3.Dot(transform.forward, _rigidBody.linearVelocity);
+            float speedFactor = Mathf.InverseLerp(0f, _carParameter.TopSpeed, Mathf.Abs(forwardSpeed));
+
+            float motorTorque = Mathf.Lerp(_carParameter.Acceleration, 0f, speedFactor);
+            float steeringRange = Mathf.Lerp(
+                _carParameter.SteeringRange,
+                _carParameter.SteeringRangeAtMaxSpeed,
+                speedFactor
+            );
+
+            bool isAccelerating = Mathf.Sign(_inputVector.y) == Mathf.Sign(forwardSpeed);
+
+            ApplyWheelForces(motorTorque, steeringRange, isAccelerating);
+        }
+
         private void ApplyWheelForces(float currentMotorTorque, float currentSteerRange, bool isAccelerating)
         {
-            foreach (var wheel in _carManager.WheelControllers)
+            foreach (var wheel in _wheelControllers)
             {
                 if (wheel.IsSteerable)
                 {
